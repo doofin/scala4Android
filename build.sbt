d95649c68764d7ab43bf3a8341215d763a271593
doofin <doofin>
2018-12-14 22:40:25 +0800

    scaloid

--- build.sbt
+++ build.sbt
@@ -2,8 +2,9 @@ enablePlugins(AndroidApp)
 enablePlugins(AndroidProtify)
 dexMulti := true
 scalaVersion := "2.11.12"
-platformTarget := "android-28"
+platformTarget := "android-21"
 libraryDependencies ++=
-  Seq("com.android.support" % "appcompat-v7" % "24.0.0")
+  Seq("org.scaloid" %% "scaloid" % "4.2")
 
 // android:packageDebug
+//"com.android.support" % "appcompat-v7" % "24.0.0",
--- src/main/scala/com/exam/MainActivity.scala
+++ src/main/scala/com/exam/MainActivity.scala
@@ -3,21 +3,29 @@ package com.exam
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.TextView
-//import android.support.v7.app.AppCompatActivity
-import android.graphics.drawable.Animatable
-import android.view.View
+import org.scaloid.common._
 
-class MainActivity extends Activity {
-    // allows accessing `.value` on TR.resource.constants
-    implicit val context: Context = this
+class MainActivity extends Activity with SContext {
 
-    override def onCreate(savedInstanceState: Bundle): Unit = {
-      super.onCreate(savedInstanceState)
-      val view = new TextView(this)
-      view.setText("halo")
-      val r = android.R
-      setTheme(r.style.Theme_DeviceDefault_Dialog_NoActionBar)
-      setContentView(view)
-    }
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    val view = new TextView(this)
+    view.setText("halo")
+
+    val ly = new SVerticalLayout {
+      new SLinearLayout {
+        SEditText() //(context,null)
+        SButton("aa")
+        SButton("b")
+      }.gravity(Gravity.RIGHT).wrap.here
+
+      STextView("Sign in") //(context,null)
+
+    }.padding(20.dip)
+
+    setContentView(ly)
+
+  }
 }
