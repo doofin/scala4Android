13cd016f248cd5c7d1b0790b188d8ae4b4098f96
doofin <doofin>
2019-01-04 14:50:36 +0800

    remove scaloid ,protify,enable those as u need

--- src/main/scala/com/exam/MainActivity.scala
+++ src/main/scala/com/exam/MainActivity.scala
@@ -5,27 +5,30 @@ import android.content.Context
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.TextView
-import org.scaloid.common._
+//import org.scaloid.common._
 
-class MainActivity extends Activity with SContext {
+class MainActivity extends Activity { //with SContext {
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     val view = new TextView(this)
-    view.setText("halo")
+    view.setText("halo00")
 
+/*
     val ly = new SVerticalLayout {
       new SLinearLayout {
         SEditText() //(context,null)
         SButton("aa")
-        SButton("b")
+        SButton("bbb")
       }.gravity(Gravity.RIGHT).wrap.here
 
       STextView("Sign in") //(context,null)
 
     }.padding(20.dip)
+*/
 
-    setContentView(ly)
+//    setContentView(ly)
+    setContentView(view)
 
   }
 }
